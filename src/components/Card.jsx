24931bfd00cdce5413a62ad6b824b32451528a83
Godfred <essiengodfred88@gmail.com>
2024-10-16 08:30:09 +0000

    added styles in Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,15 @@
 // Card component to display an icon, title, and description in a styled container
 const Card = ({ icon, title, description }) => {
     return (
-      <div>
+      <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center">
         {/* Icon displayed with a large size and custom color */}
-        <div>{icon}</div>
+        <div className="text-5xl text-[#F7CE68]">{icon}</div>
         
         {/* Title of the card, styled with custom color and font properties */}
-        <h3>{title}</h3>
+        <h3 className="text-base text-center text-[#B5651D] font-semibold mb-2">{title}</h3>
         
         {/* Description text styled with a muted color and center alignment */}
-        <p>{description}</p>
+        <p className="text-[#3E3E3E] text-sm text-center">{description}</p>
       </div>
     );
   };
