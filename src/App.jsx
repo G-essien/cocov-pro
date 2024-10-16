ba05acd14f8387589b2b2525d28e38366b9209a0
Godfred <essiengodfred88@gmail.com>
2024-10-16 08:08:39 +0000

    added the codes in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="box-border w-full h-full flex bg-gradient-to-r from-[#FBAB7E] to-[#F7CE68] justify-between flex-col">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
   )
 }
 
