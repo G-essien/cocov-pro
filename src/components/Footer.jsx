2993ef1d389b196380e47b185531037f8ca89af8
Godfred <essiengodfred88@gmail.com>
2024-10-16 08:22:19 +0000

    add global styles and base components to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,43 +8,43 @@ import { FaRegCopyright } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer>
-      <div>
-        <div>
+    <footer className="w-screen border py-3 px-2 ">
+      <div className="flex flex-col justify-center w-full items-center">
+        <div className="flex gap-2 justify-center w-full items-center">
           <div>
-            <h2>Stay Connected <span>with</span> CoCov</h2>
+            <h2 className="text-base sm:text-lg text-white ">Stay Connected <span className="text-sm italic">with</span> CoCov</h2>
           </div>
           
           {/* Social Media Icons */}
-          <div>
+          <div className="flex gap-2 justify-center items-center">
             
             {/* Instagram Icon */}
-            <a>
-              <FaInstagram />
+            <a href="https://www.instagram.com/j_gessien/" target="_blank" rel="noopener noreferrer" className="text-[#333333] hover:text-[#F7CE68] transition duration-200">
+              <FaInstagram className="text-sm md:text-lg"/>
             </a>
             
             {/* Twitter Icon */}
-            <a>
-              <FaXTwitter />
+            <a href="https://x.com/Joo_joGE" target="_blank" rel="noopener noreferrer" className="text-[#333333] hover:text-[#F7CE68] transition duration-200">
+              <FaXTwitter className="text-sm md:text-lg"/>
             </a>
             
             {/* Facebook Icon */}
-            <a>
-              <FaFacebookSquare />
+            <a href="https://web.facebook.com/?_rdc=1&_rdr" target="_blank" rel="noopener noreferrer" className="text-[#333333] hover:text-[#F7CE68] transition duration-200">
+              <FaFacebookSquare className="text-sm md:text-lg"/>
             </a>
             
             {/* LinkedIn Icon */}
-            <a>
-              <FaLinkedin />
+            <a href="https://www.linkedin.com/in/godfredessien/" target="_blank" rel="noopener noreferrer" className="text-[#333333] hover:text-[#F7CE68] transition duration-200">
+              <FaLinkedin className="text-sm md:text-lg"/>
             </a>            
 
           </div >
         </div>
 
         {/* Copyright Section */}
-        <div>
-          <FaRegCopyright />
-          <p>2024 CoCov. All rights reserved</p>
+        <div className="flex gap-2 justify-center items-center py-3">
+          <FaRegCopyright className="text-sm text-white md:text-lg"/>
+          <p className="text-base md:text-lg text-white">2024 CoCov. All rights reserved</p>
         </div>
       </div>
     </footer>
