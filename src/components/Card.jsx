4eac5f3577fea7e9be4199e9bfaed305855aad43
Godfred <essiengodfred88@gmail.com>
2024-10-16 08:27:53 +0000

    added additional base components and comments in Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,20 @@
 
-function Card() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default Card
+// Card component to display an icon, title, and description in a styled container
+const Card = ({ icon, title, description }) => {
+    return (
+      <div>
+        {/* Icon displayed with a large size and custom color */}
+        <div>{icon}</div>
+        
+        {/* Title of the card, styled with custom color and font properties */}
+        <h3>{title}</h3>
+        
+        {/* Description text styled with a muted color and center alignment */}
+        <p>{description}</p>
+      </div>
+    );
+  };
+  
+  export default Card;
+  
