60bcf69ae201c445b4ddd315adf8cf22adf4fa38
Godfred <essiengodfred88@gmail.com>
2024-10-16 09:25:16 +0000

    added additional components, functions and logics in CurrencySelector.jsx

--- src/components/CurrencySelector.jsx
+++ src/components/CurrencySelector.jsx
@@ -1,33 +1,43 @@
 
+import { HiOutlineStar, HiStar } from "react-icons/hi2";
 
-function CurrencySelector() {
+// CurrencySelector component allows user to select currencies and manage favorites
+const CurrencySelector =({currencies, currency, setCurrency, favorites, handleFavorite, title =""}) => {
 
-
-
+  // Check if the current currency is in the favorites list
+  const isFavorite = current => favorites.includes(current); // When selected as favorite currency, add to the favorite list of currency
 
   return (
     <div>
       {/* Label for the currency selector */}
-      <label></label>
+      <label htmlFor={title} >{title}</label>
     
       <div>
         {/* Dropdown to select currency */}
-        <selecton>
+        <select onChange={(e) => setCurrency(e.target.value)} value={currency}>
           {/* Loop through favorites and display them as the first options */}
-          
-              <option>
+          {favorites.map((currency) => {
+            return( 
+              <option value={currency} key={currency}>
+                {currency}
               </option>
+          )})}
           <hr />
           {/* Loop through non-favorite currencies and display them */}
-            <option>
-            </option>
-        
-        </selecton>
+          {currencies
+            .filter((c) => !favorites.includes(c))  // Filter the currencies selected as favorites from the list of currencies
+            .map((currency) => {
+            return( <option value={currency} key={currency}>
+              {currency}
+            </option>);
+          })}
+        </select>
 
           {/* Button to toggle favorite status of the currently selected currency */}
           {/* Click on the star icon to select a currency as favorite */}
-        <button> 
+        <button onClick={() => handleFavorite(currency)}> 
           {/* Display a filled star if the currency is a favorite, otherwise show an outlined star */}
+          {isFavorite(currency) ? <HiStar /> : <HiOutlineStar />} 
         </button> 
       </div>
     </div>
